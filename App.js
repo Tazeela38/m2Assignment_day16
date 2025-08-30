484fe75c3a611a9b70b0b477dd3549342b0f9674
tanzeela <tanzeelak698@gmail.com>
2025-08-29 22:49:34 -0700

    day16

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to WebTeam React Internship!</h1>
+    </div>
+  );
+}
+
+export default App;
